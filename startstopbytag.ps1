b31368ac8dc2f6b790859d4606ec4dd6bc5eabf7
Mohammad-Minaz <46309131+Mohammad-Minaz@users.noreply.github.com>
2020-12-29 14:33:59 +0530

    Update startstopbytag.ps1

--- startstopbytag.ps1
+++ startstopbytag.ps1
@@ -12,31 +12,30 @@ workflow ShutDownStartByTag
         $Shutdown 
         ) 
       
-    $connectionName = "AzureRunAsConnection"; 
-  
-    try 
-    { 
-        # Get the connection "AzureRunAsConnection " 
-        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
-  
-        "Logging in to Azure..." 
-        Add-AzureRmAccount ` 
-            -ServicePrincipal ` 
-            -TenantId $servicePrincipalConnection.TenantId ` 
-            -ApplicationId $servicePrincipalConnection.ApplicationId ` 
-            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint  
-    } 
-    catch { 
-  
-        if (!$servicePrincipalConnection) 
-        { 
-            $ErrorMessage = "Connection $connectionName not found." 
-            throw $ErrorMessage 
-        } else{ 
-            Write-Error -Message $_.Exception 
-            throw $_.Exception 
-        } 
-    } 
+    $connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+ 
+    "Logging in to Azure..."
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch 
+{
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+} 
           
       
     $vms = Find-AzureRmResource -TagName $TagName -TagValue $TagValue | where {$_.ResourceType -like "Microsoft.Compute/virtualMachines"} 
